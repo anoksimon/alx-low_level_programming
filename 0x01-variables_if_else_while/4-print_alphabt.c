9b11b71e6754c7077259eadc95b3ed6021c648a9
anoksimon <chunok4realg2@gmail.com>
2022-12-12 15:46:57 -0800

    print a to z except e & q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+
+
 /**
-** main - prints the alphabets in lowercase letters except q and e
-** return: 0
+** main -  prints the alphabets in lowercase, letters except q and e
+** Return: 0
 */
 
 int main(void)
@@ -11,11 +13,12 @@ int main(void)
 	char ch = 'a';
 
 	while (ch <= 'z')
+
 	{
 		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
-		}
+											}
 		ch++;
 	}
 	putchar('\n');
