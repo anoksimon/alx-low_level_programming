b3f0d4865bddea245ec9bf6b315cf6e1d16dd16b
anoksimon <chunok4realg2@gmail.com>
2022-12-12 19:17:45 -0800

    successful combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
-
-
 /**
- ** main - Prints all possible combinations of two different digits,
-** in ascending order, separated by a comma followed by a space.
-** Return: Always 0.
- */
+** main - program that prints all possible different combinations
+** Return: Always 0
+*/
+
+
 
 int main(void)
 
 {
-	int digit1, digit2;
+	int i;
+	int n;
+	int j;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (i = 48; i < 58; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+
+		for (n = 48; n < 58; n++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-		
-			putchar(',');
-			putchar(' ');
+			if (n > i)
+			{
+				for (j = 48; j < 58; j++)
+				{
+					if (j > n)
+					putchar(i);
+					putchar(n);
+					putchar(j);
+					if (i == 55 && n == 56 && j == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
 		}
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
