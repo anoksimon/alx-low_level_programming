25d7093d6a8a5cd43f905f9d2c86adfc9c2dfe72
anoksimon <chunok4realg2@gmail.com>
2022-12-14 14:43:08 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
-*- by Simon Chukwunwike
+** by Simon Chukwunwike
  */
 
 void times_table(void)
@@ -27,7 +27,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			} else if (k >= 10)
+			}
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
