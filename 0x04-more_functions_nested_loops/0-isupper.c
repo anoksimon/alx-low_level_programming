a52379671c8c9d7db4206595db9299d65ad389d5
anoksimon <chunok4realg2@gmail.com>
2022-12-17 22:31:15 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+/**
+* More function nested loop
+* by Simon Chukwunwike
+*/
+
+
+
+#include "main.h"
+
+
+
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
