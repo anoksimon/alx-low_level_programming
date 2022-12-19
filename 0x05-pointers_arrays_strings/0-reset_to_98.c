700aaafb93103ccce0ba2390ff9c0b92ad7bb283
anoksimon <chunok4realg2@gmail.com>
2022-12-19 10:07:22 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update the value of n to 98
+ * @n: int to update
+ * by Simon Chukwunwike
+ * Return 0
+ */
+
+void reset_to_98(int *n);
+
+{
+	*n = 98;
+	return (0);
+
+}
