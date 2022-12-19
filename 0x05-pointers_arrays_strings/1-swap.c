38443962fcece2170ea0ee1aba63f81928ec7de4
anoksimon <chunok4realg2@gmail.com>
2022-12-19 13:34:08 -0800

    swap value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the val of 2 int
+ * @a: first int
+ * @b: second int
+ * code by Simon Chukwunwike
+ * Return 0
+ */
+void swap_int(int *a, int *b)
+{
+	int = s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+
+}
+
