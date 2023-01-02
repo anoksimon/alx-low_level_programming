8fa3394d3a86547c0ba0a34b154ea7aed04b943f
anoksimon <chunok4realg2@gmail.com>
2023-01-02 05:13:36 -0800

    second commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif
